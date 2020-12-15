9073b4e16848d7e3bec71f2a6b769080f3afa105
Sangyoon Park <datumvitae@gmail.com>
2020-12-14 23:57:25 -0500

    Divide regularization term by the number of parameters (so that models with different number of parameters are somewhat comparable)

--- src/models.py
+++ src/models.py
@@ -109,7 +109,7 @@ class NeuralNet:
 
         def objective(W, t):
             squared_error = np.linalg.norm(y_train - self.forward(W, x_train), axis=1)**2
-            mean_error = np.mean(squared_error) + reg_param * np.linalg.norm(W)
+            mean_error = np.mean(squared_error) + reg_param * np.linalg.norm(W) / self.D
             return mean_error
 
         return objective, grad(objective)
@@ -383,7 +383,7 @@ class LUNA(NLM):
             # Compute L_fit
             y_train_rep = np.tile(y_train, reps=(M,1,1)) # repeat y_train with shape = dim_out x n_sample to M x dim_out x n_sample
             squared_error = np.linalg.norm(y_train_rep - self.forward(W_full, x_train), axis=1)**2
-            L_fit = np.mean(squared_error) + reg_param * np.linalg.norm(W_full)
+            L_fit = np.mean(squared_error) + reg_param * np.linalg.norm(W_full) / self.D
 
             # Comput L_diverse (#### Only works for dim_out = 1 ####)
             if self.params['dim_in'] == 1:
